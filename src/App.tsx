190aca86023307d0f99b99688fdcb3163254c813
Elias Vo <elias_vo312@mail.ru>
2026-01-10 14:28:04 +0500

    wip

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import {HelloBlock} from "./Blocks/Hello/HelloBlock";
+
+function App() {
+  return (
+    <HelloBlock/>
+  );
+}
+
+export default App;
